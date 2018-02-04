8d4ebef0053e05ced16a0d045c9054ea6c46249b
IfraA <ifraa.aijaz@gmail.com>
2018-02-04 15:35:37 -0800

    js script

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//create an array of words
+
+var namesClass = ["Ifra","Kevin","Michelle","Antonio","Ariel","Sam","Jackson","Morgan"];
+//choose word randomly
+var randNum = Math.floor(Math.random() * namesClass.length);
+var choosenWord = namesClass[randNum];
+console.log(choosenWord);
+//creates underscores based on word length
+
+//Get users guess
+
+//Check is guess is right
+
+//if right, oush to right array
+
+//if wrong push to wrong array
